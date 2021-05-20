79ef6786aade85f64d49ad5f3c0ecb85811c076b
Jamie Pace <jamiejpace@gmail.com>
2021-05-19 22:03:46 -0600

    Add hash to model b

---
+++ models/model_b.rb
@@ -0,0 +1,8 @@
+jamies_house = { owner: "Jamie Pace",
+                color: "Gray",
+                year_built: 1935,
+                number_of_bedrooms: 2.0,
+                has_dishwasher: true,
+                has_central_air: true,
+                appliances: ["dishwasher", "washer", "dryer", "stove"],
+                current_occupants: ["Diana", "Ruby", "Huxley"] }

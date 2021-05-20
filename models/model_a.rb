bb4d8c7972493cc3551e32aff445a5dea2cea20f
Jamie Pace <jamiejpace@gmail.com>
2021-05-19 22:02:01 -0600

    Add hash to model a

---
+++ models/model_a.rb
@@ -0,0 +1,8 @@
+macbook_pro = { manufacturer: "Apple",
+                name: "MacBook Pro",
+                battery_life_in_hours: 20,
+                cost: 1200,
+                has_m1_chip: true,
+                is_slow: false,
+                color_options: ["Silver", "Space Gray", "Gold"],
+                display_size_in_inches: [13, 16, 20] }

d29683a8900591b3634e199b6f8125c7980046cb
Jamie Pace <jamiejpace@gmail.com>
2021-05-19 21:59:11 -0600

    Add variables

---
+++ variables.rb
@@ -0,0 +1,9 @@
+animal = "Sea Otter"
+is_cuddly = true
+animal = "Dog"
+num_8 = 8.0
+num_3 = 3.0
+quotient = num_3 / num_8
+favorite_films = ["Nomadland", "Minari", "Burning"]
+favorite_films.shift
+puts animal.length
